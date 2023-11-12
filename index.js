396b4d78e00c8d632f7eccadc2ba3769dc78b39b
Olival <Neto@pc-adm.DUPAPI-041.upapi.local>
2023-11-12 13:39:02 -0300

    vale presente

---
+++ index.js
@@ -0,0 +1,10 @@
+let btn300 = document.getElementById('valepresente300');
+let btn200 = document.getElementById('valepresente200');
+
+btn300.addEventListener('click', function() {
+    alert("Para presenter o casal com o vale presente de R$ 300,00, faça o pix para: 82 98118-4294 (Olival) ou 82 98131-6710 (Jéssica). Ficamos muito Felizes com o seu presente. Obrigado.")
+});
+
+btn200.addEventListener('click', function() {
+    alert("Para presenter o casal com o vale presente de R$ 200,00, faça o pix para: 82 98118-4294 (Olival) ou 82 98131-6710 (Jéssica). Ficamos muito Felizes com o seu presente. Obrigado.")
+});
