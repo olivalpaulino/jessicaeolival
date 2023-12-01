426e9c422dc475189184fe92cdf0798bd9a57946
Olival <Neto@pc-adm.DUPAPI-041.upapi.local>
2023-12-01 17:03:37 -0300

    atualizando

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ let btn200 = document.getElementById('valepresente200');
 let btn150 = document.getElementById('valepresente150');
 
 btn300.addEventListener('click', function() {
-    alert("Para presenter o casal com o vale presente de R$ 300,00, faça o pix para: 82 98118-4294 (Olival) ou 82 98131-6710 (Jéssica), ou escanei o QR Code. Ficamos muito Felizes com o seu presente. Obrigado.")
+    alert("Para presenter o casal com o vale presente de R$ 300,00, faça o pix para: 82 98118-4294 (Olival) ou ritjessica@gmail.com (Jéssica), ou escanei o QR Code. Ficamos muito Felizes com o seu presente. Obrigado.")
 });
 
 btn250.addEventListener('click', function() {
-    alert("Para presenter o casal com o vale presente de R$ 250,00, faça o pix para: 82 98118-4294 (Olival) ou 82 98131-6710 (Jéssica), ou escanei o QR Code. Ficamos muito Felizes com o seu presente. Obrigado.")
+    alert("Para presenter o casal com o vale presente de R$ 250,00, faça o pix para: 82 98118-4294 (Olival) ou ritjessica@gmail.com (Jéssica), ou escanei o QR Code. Ficamos muito Felizes com o seu presente. Obrigado.")
 });
 
 btn200.addEventListener('click', function() {
-    alert("Para presenter o casal com o vale presente de R$ 200,00, faça o pix para: 82 98118-4294 (Olival) ou 82 98131-6710 (Jéssica), ou escanei o QR Code. Ficamos muito Felizes com o seu presente. Obrigado.")
+    alert("Para presenter o casal com o vale presente de R$ 200,00, faça o pix para: 82 98118-4294 (Olival) ou ritjessica@gmail.com (Jéssica), ou escanei o QR Code. Ficamos muito Felizes com o seu presente. Obrigado.")
 });
 
 btn150.addEventListener('click', function() {
-    alert("Para presenter o casal com o vale presente de R$ 150,00, faça o pix para: 82 98118-4294 (Olival) ou 82 98131-6710 (Jéssica), ou escanei o QR Code. Ficamos muito Felizes com o seu presente. Obrigado.")
+    alert("Para presenter o casal com o vale presente de R$ 150,00, faça o pix para: 82 98118-4294 (Olival) ou ritjessica@gmail.com (Jéssica), ou escanei o QR Code. Ficamos muito Felizes com o seu presente. Obrigado.")
 });
